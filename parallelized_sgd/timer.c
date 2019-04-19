fcfb29acade641206306a4d986b7fef86b13b966
Michael Stecklein <michaelrstecklein@gmail.com>
2019-04-19 12:27:07 -0500

    First commit, basic files

---
+++ parallelized_sgd/timer.c
@@ -0,0 +1,73 @@
+#include "timer.h"
+
+
+//  This module uses clock_gettime to record real times and 
+//  uses getrusage to record user/sys times. See their man
+//  pages for more info:
+//  http://man7.org/linux/man-pages/man2/clock_gettime.2.html
+//  http://man7.org/linux/man-pages/man2/getrusage.2.html
+
+
+int timer_initialize(timer_t *timer, int scope) {
+	timer->status = TIMER_STATUS_PAUSED;
+	timer->scope = scope;
+	timer->real_cumulative = 0;
+	timer->user_cumulative = 0;
+	timer->sys_cumulative = 0;
+	return 0;
+}
+
+
+int timer_start(timer_t *timer) {
+	if (timer->status != TIMER_STATUS_PAUSED)
+		return -1;
+	// Get start times
+	int rc;
+	rc = clock_gettime(CLOCK_MONOTONIC, &(timer->start_time_tspec));
+	if (rc)
+		return rc;
+	int who = RUSAGE_SELF; // TODO (timer->scope == TIMER_SCOPE_PROCESS) ? RUSAGE_SELF : RUSAGE_THREAD;
+	rc = getrusage(who, &timer->start_time_rusg);
+	if (rc)
+		return rc;
+	// Timer is now "running"
+	timer->status = TIMER_STATUS_RUNNING;
+	return 0;
+}
+
+
+int timer_pause(timer_t *timer) {
+	if (timer->status != TIMER_STATUS_RUNNING)
+		return 0;
+	// Read current times
+	int rc;
+	struct rusage curr_rusg;
+	struct timespec curr_tspec;
+	rc = clock_gettime(CLOCK_MONOTONIC, &curr_tspec);
+	if (rc)
+		return rc;
+	int who = RUSAGE_SELF; // TODO (timer->scope == TIMER_SCOPE_PROCESS) ? RUSAGE_SELF : RUSAGE_THREAD;
+	rc = getrusage(who, &curr_rusg);
+	if (rc)
+		return rc;
+	// Add diff since start times to the accumulator
+	long long real_diff = (curr_tspec.tv_sec - timer->start_time_tspec.tv_sec)*1000000000L + (curr_tspec.tv_nsec - timer->start_time_tspec.tv_nsec);
+	long long user_diff = (curr_rusg.ru_utime.tv_sec - timer->start_time_rusg.ru_utime.tv_sec)*1000000L + (curr_rusg.ru_utime.tv_usec - timer->start_time_rusg.ru_utime.tv_usec);
+	long long sys_diff = (curr_rusg.ru_stime.tv_sec - timer->start_time_rusg.ru_stime.tv_sec)*1000000L + (curr_rusg.ru_stime.tv_usec - timer->start_time_rusg.ru_stime.tv_usec);
+	timer->real_cumulative += real_diff;
+	timer->user_cumulative += user_diff;
+	timer->sys_cumulative  += sys_diff;
+	// Timer is now "paused"
+	timer->status = TIMER_STATUS_PAUSED;
+	return 0;
+}
+
+
+int timer_get_stats(timer_t *timer, timerstats_t *stats) {
+	if (timer->status != TIMER_STATUS_PAUSED)
+		return -1;
+	stats->real = (timer->real_cumulative) / ((double) 1000000000L);
+	stats->user = (timer->user_cumulative) / ((double) 1000000L);
+	stats->sys  = (timer->sys_cumulative)  / ((double) 1000000L);
+	return 0;
+}
---
+++ parallelized_sgd/timer.h
@@ -0,0 +1,60 @@
+/*
+ * Timer
+ *   Object to measure runtime information.
+ *   "real" time is the wall-clock time, as observed externally.
+ *   "user" time is the amount of CPU time spent in user mode.
+ *   "sys"  time is the amount of CPU time spent in system/kernel mode.
+ *   "process" scope tracks the above stats for the entire process.
+ *   "thread" scope tracks the above stats for the calling thread.
+ */
+
+#ifndef _timer_hpp_
+#define _timer_hpp_
+
+
+#include <sys/time.h>
+#include <sys/resource.h>
+
+
+#define TIMER_STATUS_RUNNING	1
+#define TIMER_STATUS_PAUSED		2
+#define TIMER_SCOPE_PROCESS		5
+#define TIMER_SCOPE_THREAD		6
+typedef struct _timer_t {
+	int status; // running/paused
+	int scope; // process/thread
+	struct timespec start_time_tspec; // real times
+	struct rusage start_time_rusg; // user/sys times
+	long long real_cumulative; // nanoseconds
+	long long user_cumulative; // microseconds
+	long long sys_cumulative; // microseconds
+} timer_t;
+
+
+typedef struct _timerstats_t {
+	// The "wall clock" time, as viewed external to the process.
+	double real; // seconds
+	// The amount of CPU time spent in user mode.
+	double user; // seconds
+	// The amount of CPU time spent in system (kernel) mode.
+	double sys; // seconds
+} timerstats_t;
+
+
+// Initializes the timer struct
+int timer_initialize(timer_t *timer, int scope);
+
+// Starts/resumes the timer.
+//    The timer must be paused.
+int timer_start(timer_t *timer);
+
+// Pauses/stops the timer.
+//    If timer is already paused, no op occurs.
+int timer_pause(timer_t *timer);
+
+// Returns the timer's stats at the current state.
+//    The timer must be paused.
+int timer_get_stats(timer_t *timer, timerstats_t *stats);
+
+
+#endif // _timer_hpp_
